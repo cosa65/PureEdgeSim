530431265249ce421ba83deb5202036783204771
cosa65 <martin@biomage.net>
2024-08-24 16:05:27 -0300

    Rename

--- PureEdgeSim/tesis/Tesis.java
+++ PureEdgeSim/tesis/Tesis.java
@@ -64,7 +64,7 @@ public class Tesis {
 	// Below is the path for the settings folder of this example
 //	 private static String settingsPath = "PureEdgeSim/tesis/settings/Tesis_settings/";
 //	 private static String settingsPath = "PureEdgeSim/tesis/settings/10_nodes_settings/";
-	private static String settingsPath = "PureEdgeSim/tesis/settings/test_2_nodes_mobile/";
+	private static String settingsPath = "PureEdgeSim/tesis/settings/test_2_nodes_mobile_assymetric/";
 
 	// The custom output folder is
 	private static String outputPath = "PureEdgeSim/tesis/Tesis_output/";
